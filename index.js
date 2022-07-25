03d0533e5d1bc8280812faf39d882f22a310b987
fedeargfyron <fedemgs15@gmail.com>
2022-07-25 19:29:28 -0300

    Se agrega lógica para crear la tabla de partidas guardadas e ingresar a una

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 onload = () => {
     document.getElementById("newGameBtn").addEventListener("click", newGame)
-    document.getElementById("loadGameBtn").addEventListener("click", loadGame)
+    document.getElementById("loadGameBtn").addEventListener("click", loadGamesTable)
 }
 
 const newGame = () => {
@@ -8,8 +8,26 @@ const newGame = () => {
     window.location = "game.html";
 }
 
-const loadGame = () => {
-    localStorage.setItem("loadedGame", 3);
+const loadGamesTable = () => {
+    let table = document.getElementById("loadGames-table");
+    table.innerHTML = "";
+    let games = JSON.parse(localStorage.getItem("games"), '[]').filter(x => x.completed === false);
+
+    games.forEach(x => {
+        let row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${x.id}</td>
+            <td>${x.user}</td>
+            <td>${x.date}</td>
+            <td>${x.hour}:${x.min}:${x.sec}</td>`;
+        row.id = x.id;
+        row.addEventListener("click", loadGame);
+        table.appendChild(row);
+    });
+}
+
+const loadGame = (e) => {
+    let id = e.target.parentElement.id;
+    localStorage.setItem("loadedGame", id);
     window.location = "game.html";
-    //Id, Jugador, Fecha, Tiempo
 }
