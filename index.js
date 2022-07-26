71687d2309fa34fcf7f9f3093be56120c12512f5
fedeargfyron <fedemgs15@gmail.com>
2022-07-26 16:14:24 -0300

    Se agregan estilos a la tabla y se agrega encabezado a la misma

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ const newGame = () => {
 }
 
 const loadGamesTable = () => {
+    document.getElementById("load-modal").classList.remove("invisible");
     let table = document.getElementById("loadGames-table");
     table.innerHTML = "";
     let games = JSON.parse(localStorage.getItem("games"), '[]').filter(x => x.completed === false);
+    let header = document.createElement("tr");
+    header.innerHTML = `
+            <th>Id</th>
+            <th>Usuario</th>
+            <th>Fecha</th>
+            <th>Tiempo</th>`;
+    table.appendChild(header);
 
     games.forEach(x => {
         let row = document.createElement("tr");
         row.innerHTML = `
             <td>${x.id}</td>
-            <td>${x.user}</td>
+            <td>${x.user.toUpperCase()}</td>
             <td>${x.date}</td>
             <td>${x.hour}:${x.min}:${x.sec}</td>`;
         row.id = x.id;
